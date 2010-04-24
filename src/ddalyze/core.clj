dd791cd89109746b7d87f3b88f261a2aea5880a5
Alan Shields <alan@alan.shields.name>
2010-04-24 15:39:12 -0700

    Compare map analyses

--- src/ddalyze/core.clj
+++ src/ddalyze/core.clj
@@ -18,3 +18,9 @@
       (println "Result cost: " (shortest-map-path-cost best-towers-map))
       (println (show-map-compares (draw-shortest-path best-towers-map) best-towers-map)))))
 
+(defn compare-map-analyses [file method1 method2]
+  (let [current-map (map-from-map-file file)
+        best-towers1 (method1 current-map)
+        best-towers2 (method2 current-map)]
+    (println (format "Analyses done. Method1: %.2f vs Method2: %.2f" (float (shortest-map-path-cost best-towers1)) (float (shortest-map-path-cost best-towers2))))
+    (println (show-map-compares best-towers1 best-towers2))))
