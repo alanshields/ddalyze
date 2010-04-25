aad3dca70f17c67569885ae0fffa69d9c5023a43
Alan Shields <alan@alan.shields.name>
2010-04-25 04:38:00 -0700

    Searching for the best genetic search parameters

--- project.clj
+++ project.clj
@@ -1,5 +1,6 @@
 (defproject ddalyze "0.0.1-SNAPSHOT"
   :description "Desktop Defender analysis tools"
   :dependencies [[org.clojure/clojure "1.1.0"]
-                 [org.clojure/clojure-contrib "1.1.0"]]
+                 [org.clojure/clojure-contrib "1.1.0"]
+                 [incanter "1.2.3-SNAPSHOT"]]
   :dev-dependencies [[leiningen/lein-swank "1.2.0-SNAPSHOT"]])
--- src/ddalyze/core.clj
+++ src/ddalyze/core.clj
@@ -5,14 +5,16 @@
         ddalyze.map
         ddalyze.parse
         ddalyze.search
-        ddalyze.util))
+        ddalyze.util
+        [clojure.contrib.combinatorics :only (cartesian-product)]
+        [incanter.stats :only (sd)]))
 
 (def *simple-map-file* "/Users/alanshields/code/desktop_defender/maps/basic.map")
 (def *simple-map* (map-from-map-file *simple-map-file*))
 
-(defn analyze-map-file [file]
+(defn analyze-map-file [file method]
   (let [current-map (map-from-map-file file)
-        best-towers-map (best-towers-for-creeps current-map)]
+        best-towers-map (method current-map)]
     (when *debug-output*
       (println)(println)
       (println "Result cost: " (shortest-map-path-cost best-towers-map))
@@ -24,3 +26,25 @@
         best-towers2 (method2 current-map)]
     (println (format "Analyses done. Method1: %.2f vs Method2: %.2f" (float (shortest-map-path-cost best-towers1)) (float (shortest-map-path-cost best-towers2))))
     (println (show-map-compares best-towers1 best-towers2))))
+
+(defn find-best-genetic-search-params [file method]
+  (let [current-map (map-from-map-file file)
+        score-to-params
+        (map (fn [[max-generations descendants-from-each number-of-fit-to-keep :as params]]
+               (binding [*ddebug-output* false
+                         *max-generations* max-generations
+                         *descendants-from-each* descendants-from-each
+                         *number-of-fit-to-keep* number-of-fit-to-keep]
+                 (println "trying max" max-generations "desc" descendants-from-each "number-of-fit" number-of-fit-to-keep)
+                 (cons
+                  (shortest-map-path-cost (method current-map))
+                  params)))
+             (cartesian-product '(50)
+                                '(5 30)
+                                '(20 100)))
+        best-scores (sort-by #(first %1) > score-to-params)]
+    (println "best:" (first best-scores))
+    (println "next best:" (second best-scores))
+    (println "sd:" (sd (map first best-scores)))
+    (println best-scores)))
+  
